e37b4465d9ff58063e8bc6dfadf05cb9efaee425
taikool2121996 <taikool2121996@gmail.com>
2024-05-01 22:19:39 +0700

    embed gg map

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -279,10 +279,9 @@ export default function Contact() {
       <div className="container-fluid relative">
         <div className="grid grid-cols-1">
           <div className="w-full leading-[0] border-0">
-            {/* <iframe  title='google' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin" style={{border:0}} className="w-full h-[500px]" allowFullScreen></iframe> */}
             <iframe
               title="google"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.126202726886!2d106.65230857655803!3d10.801644989348677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529379073c071%3A0xb43136b2f837555b!2zMThlIMSQLiBD4buZbmcgSMOyYSwgUGjGsOG7nW5nIDQsIFTDom4gQsOsbmgsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1714576204853!5m2!1svi!2s"
               style={{ border: 0 }}
               className="w-full h-[500px]"
               allowFullScreen
